07d753e4983d0ee58461004a1f3b7be0709bda96
marb403 <marbadias97@gmail.com>
2018-04-07 19:16:17 +0200

    proves amb el frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,118 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import Display from './Display';
+//import ButtonPanel from './ButtonPanel';
+//import calculate from '../logic/calculate';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+
+        <div className="App">
+            <header className="App-header">
+                <h1 className="App-title">Welcome USER!</h1>
+            </header>
+            <body className="App-body">
+                <p className="App-intro">
+                    Hola! proves de reactjs!
+                </p>
+
+                <div className="game">
+                    <div className="game-board">
+                        <Board />
+                    </div>
+                    <div className="game-info">
+                    </div>
+                </div>
+
+
+            </body>
+        </div>
+
+
+
+      /*<div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      </div>*/
     );
   }
 }
+
+
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
+    }
+
+    render() {
+        const status = 'Next player: ' +
+            (this.state.xIsNext ? 'X' : 'O');
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+
+
+
+
+
 
 export default App;
