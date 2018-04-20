09c1c9f244ac799baa24158f8c406b1503107559
xavi <xavier.marrugat@est.fib.upc.edu>
2018-04-20 13:23:03 +0200

    Solved error: 'return {} when expected 2' but now getting 'Uncaught TypeError: Cannot read property 'constructor' of undefined'

--- test/etherTest.js
+++ test/etherTest.js
@@ -1,27 +1,60 @@
 const Ethervote = artifacts.require('./Ethervote.sol')
 
-contract('Ethervote', function ([owner, donor]) {
-	let ethervote;
+contract('Ethervote', function (accounts) {
+	let ethervote
+
+    let owner_address = accounts[0]
+    let voter_1 = accounts[1]
 
 	beforeEach('setup contract for each test', async function () {
-        ethervote = await Ethervote.new("Test 1", 5)
+     ethervote = await Ethervote.new("Test 1", 5)
 	})
 
+    //accounts[0] es sempre el owner
     it('has an owner', async function() {
-    	assert.equal(await ethervote.owner(), owner)
-    })
-
-    it('create proposal', async function() {
-    	await ethervote.newProposal("proposal_test_1", "First proposal test description", 300, owner, "yes")
-    	assert.equal(ethervote.proposals.size(), 1)
+    	assert.equal(await ethervote.owner(), accounts[0])
     })
 
     it('add voter', async function() {
-    	await ethervote.addVoter(owner, 2)
-    	//assert.equals(ethervote.census.size(), 1)
-    	
-
-
-    	assert.equal(ethervote.getPrivilege(ethervote.owner()), null)
+        await ethervote.addVoter(accounts[1], 2, {from: owner_address})  
     })
+
+    it('has privilege', async function() {
+        await ethervote.addVoter(voter_1, 2, {from: owner_address})
+        let priv = await ethervote.getPrivilege(voter_1)
+        assert.equal(priv, 2)
+    })
+
+    /****************************************************************************************/
+    //Getting error in assertion: "return {} when expected 2"
+
+    it('has changed privilege', async function() {
+        await ethervote.addVoter(voter_1, 2, {from: owner_address})
+        let priv_1 = await ethervote.getPrivilege(voter_1)
+        assert.equal(priv_1, 2)
+
+        await ethervote.changePrivilege(voter_1, 1, {from: owner_address})
+        let priv_2 = await ethervote.getPrivilege(voter_1)
+        assert.equal(priv_2, 1)
+    })
+
+    it('has deleted user', async function() {
+        await ethervote.addVoter(voter_1, 2, {from: owner_address})
+        let priv = await ethervote.getPrivilege(voter_1)
+        assert.equal(priv, 2)
+
+        await ethervote.deleteVoter(voter_1, {from: owner_address})
+        priv = await ethervote.getPrivilege(voter_1)
+        assert.equal(priv, 0)
+    })
+    /****************************************************************************************/
+
+
+    it('create proposal', async function() {
+        await ethervote.addVoter(voter_1, 2, {from: owner_address})
+        await ethervote.newProposal("proposal_test_1", "First proposal test description", 
+            300, voter_1, "yes", {from: voter_1})
+    })
+
+    
 })
