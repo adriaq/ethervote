1b6fb681d5bb827ed934c946b26d0ef3598da87f
marb403 <marbadias97@gmail.com>
2018-05-03 10:24:15 +0200

    bootsrap instalat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import construction from './contruction.gif';
-
+import ListGroup from 'react-bootstrap/lib/ListGroup'
+import ListGroupItem from 'react-bootstrap/lib/ListGroup'
 import './App.css';
 import Menu, {SubMenu, MenuItem} from 'rc-menu';
 
@@ -42,32 +43,31 @@ class App extends Component {
 
 
 class MenuContainer extends Component {
+
+    alertClicked() {
+    alert('Has seleccionat una votació');
+    }
+
     render() {
         return (
             <div>
                 <div >
                     <p className="TitolMenu">Select an open vote</p>
                     <ul className="Menu">
-                        <Button bsStyle="primary" />
-                        <li className="llista" >USA PRESIDENT</li>
-                        <li className="llista" >CEO ELECTION</li>
-                        <li className="llista" >MY DOG'S NAME</li>
-                        <li className="llista">NEW HARRY POTTER FILM?</li>
-                        <li className="llista">SHOULD TAXES INCREASE?</li>
-                        <li className="llista" >GRUPS FESTA FIB 2018</li>
+                        <ListGroup>
+                        <ListGroupItem onClick={this.alertClicked()} href="#" active className="llista" >USA PRESIDENT</ListGroupItem>
+                            <ListGroupItem className="llista" >CEO ELECTION</ListGroupItem>
+                            <ListGroupItem className="llista" >MY DOG'S NAME</ListGroupItem>
+                        <ListGroupItem className="llista">NEW HARRY POTTER FILM?</ListGroupItem>
+                        <ListGroupItem className="llista">SHOULD TAXES INCREASE?</ListGroupItem>
+                        <ListGroupItem className="llista" >GRUPS FESTA FIB 2018</ListGroupItem>
+                            </ListGroup>
                     </ul>
                 </div>
             </div>
         );
     }
-}
 
-class Button extends Component {
-    render() {
-        return (
-            <li className="llista" >Hola, soc un boto pero encara no faig res</li>
-        );
-    }
  }
 
 
