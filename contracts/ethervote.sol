757ae9a696deb20449f4e284adffcda7a4c8808d
Adrià Quesada <adria.quesada.alacid@est.fib.upc.edu>
2018-05-05 20:09:24 +0200

    smart contract acabat i amb documentacio

--- contracts/ethervote.sol
+++ contracts/ethervote.sol
@@ -29,10 +29,10 @@ contract ethervote {
     mapping(address => Voter) private census;  //conjunt de adreces que podran votar
     address[] private voters;
     mapping (int => Proposal) private proposals;       //conjunt de proposals a votar
-    int public n_proposals;
+    int private n_proposals;
     uint public creationTime;                  //Quan es va crear el contracte
     string public name;                        //Nom de la organització
-    uint defaultVotingTime;                    //Temps per defecte de les votacions
+    uint public defaultVotingTime;                    //Temps per defecte de les votacions
     //Fi Variables Globals
 
 
@@ -54,68 +54,71 @@ contract ethervote {
       _;
     }
     //Creadora
-    constructor(string _name, uint _defaultVotingTime) public {
+    constructor(string _name, uint _defaultVotingTime) public  {
       owner = msg.sender;
+      n_proposals = 0;
       creationTime = now;
       name = _name;
       defaultVotingTime = _defaultVotingTime;
      }
     //L'esdeveniment saltara amb el resultat de si s'ha afegit be o no el votant. Una adreça 0 significa que hi ha hagut algun error
-    event addVoterResult(address _voter);
+    //event addVoterResult(address _voter);
 
     //Afegir votant
-    function addVoter(address _voter, int _privilege) onlyOwner public {
-      if((_privilege >= 1) && (_privilege <=3)) {
-        census[_voter] = Voter({privilege: _privilege});
-        voters.push(_voter);
-        emit addVoterResult(_voter);
+    function addVoter(address _voter, int _privilege) onlyOwner public returns(bool) {
+      if((_privilege >= 1) &&
+        (_privilege <=3) &&
+        (census[_voter].privilege == 0)){
+            census[_voter] = Voter({privilege: _privilege});
+            voters.push(_voter);
+            return true;
+            //emit addVoterResult(_voter);
       }
-      emit addVoterResult(address(0));
+      return false;
+      //emit addVoterResult(address(0));
     }
 
     function getPrivilege(address _voter) public view returns (int){
       return census[_voter].privilege;
     }
 
-    event changePrivilegeResult(bool);
-    function changePrivilege(address _voter, int _privilege) onlyOwner public {
+    function changePrivilege(address _voter, int _privilege) onlyOwner public returns(bool) {
       census[_voter].privilege = _privilege;
-      emit changePrivilegeResult(true);
+      return true;
     }
 
-    event deleteVoterResult(bool);
-    function deleteVoter(address _voter) onlyOwner public {
+    function deleteVoter(address _voter) public onlyOwner {
         census[_voter].privilege = 0;
-        emit deleteVoterResult(true);
     }
+
     function getNumberOfVoters() public view returns (int){
         return int(voters.length);
     }
-    function newProposal(string _name, string _description, uint _votingTime, address _creator) canCreate(_creator) public returns(bool succes) {
-        int proposalID = ++n_proposals;
+    function newProposal(string _name, string _description) canCreate(msg.sender) public returns(int) {
         if( (bytes(_name).length > 0) &&
-            (bytes(_description).length > 0) &&
-            (_votingTime > 0)
+            (bytes(_description).length > 0)
         ){
+            int proposalID = ++n_proposals;
             proposals[proposalID].creator = _creator;
             proposals[proposalID].name = _name;
             proposals[proposalID].description = _description;
-            proposals[proposalID].creator = _creator;
-            proposals[proposalID].votingDeadline = now+_votingTime;
+            proposals[proposalID].creator = msg.sender;
+            proposals[proposalID].votingDeadline = now + defaultVotingTime;
             proposals[proposalID].exists = true;
-            return true;
-        } else return false;
+            return proposalID;
+        } else return -1;
     }
 
-    function addOption(int _proposalID, string _name, string _description) public onlyOwner onlyCreator(_proposalID) returns(bool)  {
+    function addOption(int _proposalID, string _name, string _description) public onlyCreator(msg.sender) returns(int)  {
         if( (bytes(_name).length > 0) && //si el nom no esta buit
             (proposals[_proposalID].exists) //si la proposal existeix
         ){
-            int n_options = proposals[_proposalID].n_options;
-            proposals[_proposalID].options[++n_options].name = _name;
-            proposals[_proposalID].options[++n_options].description  = _description;
-            return true;
-        } else return false;
+
+            int n_option = ++proposals[_proposalID].n_options;
+            proposals[_proposalID].options[n_option].name = _name;
+            proposals[_proposalID].options[n_option].description  = _description;
+            return n_option;
+        } else return -1;
     }
     function getNumberOfOptions(int _proposalID) public view returns(int) {
       if(proposals[_proposalID].exists){
@@ -136,23 +139,39 @@ contract ethervote {
           return proposals[_proposalID].options[_n_option].name;
       } else return "";
     }
-    function getOptionName(int _proposalID, int _n_option) public view returns(string) {
-      if(proposals[_proposalID].exists &&
-         proposals[_proposalID].options[_n_option].exists
-        ){
-          return proposals[_proposalID].options[_n_option].description;
-      } else return "";
+    function hasEnded(int _proposalID) public view returns(bool) {
+      if(proposals[_proposalID].exists){
+          return now > proposals[_proposalID].votingDeadline;
+      }
     }
-
-//    function deleteProposal() public{}
       function getNumberOfProposals() public view returns (int) {
         return n_proposals;
       }
-      //function getProposal(int _proposalID) public returns(int, address, string, string, uint){}
 
+    function vote(int _proposalID, int _option) public canVote(msg.sender) returns(bool){
+        if(proposals[_proposalID].exists &&
+           proposals[_proposalID].options[_option].exists &&
+           (now < proposals[_proposalID].votingDeadline)
+       ) {
+           if(!hasVoted(msg.sender,_proposalID)) {
+               proposals[_proposalID].options[_option].votes.push(msg.sender);
+           }
+       }
+    }
 
-//    function vote(int proposalID, string option) public returns(int){}
-//    function deleteVote(int proposalID) public{}
-//    function getVote(int proposalID) public{}
+    function hasVoted(address _voter, int _proposalID) public view canVote(_voter) returns(bool) {
+        if (proposals[_proposalID].exists) {
+            int n_options = getNumberOfOptions(_proposalID);
+            int n_votes;
+            bool voted = false;
+            for (int opt=0; opt < n_options; ++opt) {
+                n_votes = getNumberOfVotes(_proposalID,opt);
+                for (int i=0; i < n_votes; ++i) {
+                    if(proposals[_proposalID].options[opt].votes[uint(i)] == _voter) voted = true;
+                }
+            }
+            return voted;
+        }
+    }
 
 }
