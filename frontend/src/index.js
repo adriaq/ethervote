f6999489276e7818268f503637a550e00e6c2504
Alba <alba.canete.garrucho@est.fib.upc.edu>
2018-05-08 13:17:53 +0200

    canvi pantalla utilitzant router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {render} from 'react-dom';
 
 import App from './App';
 import Login from './Login'
-//import Game from './App';
-import registerServiceWorker from './registerServiceWorker';
-import Views from "./Views";
-//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-//import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 
+const Root = () => {
+    return (
+        <div className="container">
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/app" component={App}/>
+                    <Route path="/login" component={Login}/>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    )
+}
 
-ReactDOM.render(
-    <Views/>,
-    //<Views pathname={location.pathname}/>,
-   // <Login />,
-    //<App/>,
-    document.getElementById('root')
-);
-    registerServiceWorker();
+ReactDOM.render(<Root />, document.getElementById('root'));
