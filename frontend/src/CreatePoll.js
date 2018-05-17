46b93ca219bc796a2da878738b78b2177a616f6f
Alba <alba.canete.garrucho@est.fib.upc.edu>
2018-05-17 16:49:49 +0200

    esquelet component CreatePoll

--- frontend/src/CreatePoll.js
+++ frontend/src/CreatePoll.js
@@ -1,8 +1,37 @@
-import Component from 'react';
-
+import React, {Component} from 'react';
+import {Button} from 'reactstrap';
 
 class CreatePoll extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
+
+
+    render() {
+
+        return (
+
+            <div>
+                <nav class="navbar navbar-expand-lg custom-navbar">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <a class="navbar-brand"> New boss election! </a>
+                        </div>
+                        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                            <ul class="nav navbar-nav">
+                                <li><a href="#"> Help </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+                <Button className="enrere" color="danger" href="/"> Back </Button>
+
+            </div>
+        );
+    }
 }
 
 /*
