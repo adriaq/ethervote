6078e4e0f433efafa633b075cdd5984c353fb7d8
Alba <alba.canete.garrucho@est.fib.upc.edu>
2018-05-18 11:18:09 +0200

    help fet

--- frontend/src/App2.js
+++ frontend/src/App2.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Button} from 'reactstrap';
 import {ListGroup} from 'reactstrap';
 import {ListGroupItem} from 'reactstrap';
+import {Glyphicon, ButtonToolbar, DropdownButton, MenuItem} from 'react-bootstrap';
 import './styles/App.css';
 
 
@@ -37,24 +38,38 @@ class App extends Component {
             <nav class="navbar navbar-expand-lg custom-navbar">
                     <div class="container-fluid">
                         <div class="navbar-header">
-                            <a class="navbar-brand" href="/"> Welcome UserName! </a>
+                            <a class="navbar-brand" href="/"> Welcome to Ethervote! </a>
                         </div>
                         <div class="collapse navbar-collapse" id="collapsibleNavbar">
                             <ul class="nav navbar-nav">
-                                <li><a href="#"> Help </a></li>
+                                <ButtonToolbar>
+                                    <DropdownButton title="Help" pullRight id="dropdown-no-caret">
+                                        <MenuItem eventKey="1">
+                                            <p> You can create a new poll in the <i> Create poll </i> division </p>
+                                        </MenuItem>
+                                        <MenuItem divider />
+                                        <MenuItem eventKey="2">
+                                            <p> You can see a list of the polls you have access to vote in the
+                                                <br/> <i> Open polls </i> division </p>
+                                        </MenuItem>
+                                        <MenuItem divider />
+                                        <MenuItem eventKey="3">
+                                            <p> You can see a list of the polls in which you have participated
+                                                <br/>  in the <i> Results </i> division </p>
+                                        </MenuItem>
+                                    </DropdownButton>
+                                </ButtonToolbar>
                             </ul>
                         </div>
                     </div>
                 </nav>
 
                 <div class="row">
-                    <div class="col-lg-6">
+                    <div class="col-lg-10">
                         <h3> CREATE POLL </h3>
-                        <p> Explicació de crear una votació </p>
-                    </div>
-
-                    <div class="col-lg-6">
-                        <Button color="primary"  href="/createPoll" > Create poll </Button>
+                        <p id="create-explanation"> Create a poll and add the public key of the users you want
+                                them to participate.  </p>
+                        <Button color="primary" className="createPoll" href="/createPoll" > Create poll </Button>
                     </div>
                 </div>
 
@@ -65,7 +80,8 @@ class App extends Component {
 
                         <ListGroup>
                             {this.state.votations.map( v =>
-                                <ListGroupItem tag="a" href={"/openPolls"+ '#' +v.id} key={v.id}>{v.name}
+                                <ListGroupItem tag="a" key={v.id}>
+                                    {v.name} <a href={"/openPolls" + v.id}> <Glyphicon glyph="zoom-in" /> </a>
                                 </ListGroupItem>)}
                         </ListGroup>
                     </div>
@@ -75,15 +91,16 @@ class App extends Component {
                         <ListGroup>
                             <ListGroup>
                                 {this.state.closeVotations.map( v =>
-                                    <ListGroupItem tag="a"  href={"/pollsResult"+ '#' +v.id} key={v.id}>{v.name}
+                                    <ListGroupItem tag="a" key={v.id}>
+                                        {v.name} <a href={"/pollsResult" + v.id}> <Glyphicon glyph="zoom-in" /> </a>
                                     </ListGroupItem>)}
                             </ListGroup>
                         </ListGroup>
                     </div>
                 </div>
 
-            <div class="center">
-                <Button color="danger"  href="/admin" > Back </Button>
+            <div>
+                <Button color="danger" className="back" href="/admin" > Back </Button>
             </div>
 
 
