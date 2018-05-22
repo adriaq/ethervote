059fba201cf5e8e50ff852fb9d650ad16ca05a4d
Jorge Ferrer Rodríguez <jferrer991@gmail.com>
2018-05-22 19:48:02 +0200

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -37,7 +37,37 @@ export default class Header extends Component{
               </div>
           </nav>
 
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="/">Ethervote</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
 
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/admin">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/admin">Open polls</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/createPoll">New Poll</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/logout">Log out</a>
+                </li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+            </div>
+          </nav>
+    
       </div>
 
       );
