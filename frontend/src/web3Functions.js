b1202d61855df3aea27efbc09c626b796d4d5f81
xavi <xavier.marrugat@est.fib.upc.edu>
2018-05-23 23:41:31 +0200

    first fix

--- frontend/src/web3Functions.js
+++ frontend/src/web3Functions.js
@@ -3,7 +3,7 @@ import React from 'react';
 var ethervote;
 /************************ USER **********************/
 
-module.exports.getUserOpenedPolls = async () => {
+module.exports.getOpenedPolls = async () => {
         let num_proposals = await ethervote.getNumberOfProposals();
         let proposals = [];
         for(let i=1; i<=num_proposals; ++i) {
@@ -26,10 +26,10 @@ module.exports.getUserOpenedPolls = async () => {
                 proposals.push(p);
             }
         }
-
+        //return proposals
 };
 
-module.exports.getUserClosedPolls = async() => {
+module.exports.getClosedPolls = async() => {
     let num_proposals = await ethervote.getNumberOfProposals(); //BigNumber { s: x, e: y, c: [ z ] } -> c es el que es necessita
     let proposals = [];
     for(let i=0; i<num_proposals; ++i) {
@@ -44,6 +44,7 @@ module.exports.getUserClosedPolls = async() => {
                 let option_name = await ethervote.getOptionName(i, j); //retorna tal qual el nom
                 let option_description = await ethervote.getOptionDescription(i, j); //retorna tal qual la descripció
                 let option_votes = await ethervote.getNumberOfVotes(i, j); //BigNumber { s: x, e: y, c: [ z ] } -> c es el que es necessita
+                
                 let o = {"name": option_name, "description": option_description, "votes": option_votes};
                 options.push(o);
             }
@@ -52,18 +53,21 @@ module.exports.getUserClosedPolls = async() => {
             proposals.push(p);
         }
     }
+    //return proposals
 };
 
 module.exports.vote = async(key, id, option) => {
     let v = false;
-    let voted = await ethervote.hasVoted(key, id);
-    if(!voted) { //retorna tal qual true or false
+    let voted = await ethervote.hasVoted(key, id); //retorna tal qual true or false
+    if(!voted) { 
         await ethervote.vote(id, option); //no retorna res important
-        v = true;
+
+        v = await ethervote.hasVoted(key, id); 
     }
 
-
-    //retornar v
+    return v;
+    //v si false hi ha hagut un error, 
+    //ja sigui xq ya ha votat o perque no ha pogut votar per algo extrany
 };
 
 
@@ -73,34 +77,49 @@ module.exports.vote = async(key, id, option) => {
  * POST: S'ha creat una nova votació amb l'smart contract instanciat
  */
 module.exports.newPoll = async(name, description) => {
+    var p1 = await getNumberOfProposals();
     await ethervote.newProposal(name, description);
+    var p2 = await getNumberOfProposals();
+
+    return (p1+1 == p2);
     // TODO: saber com retorna si s'ha pogut crear l'event
+
 };
 
+/************************* AQUESTES FUNCIONS JA ESTAN A USER ************************/
 /**
  * PRE: Administador entra a la web
  * POST: json amb un llistat de les votacions obertes que ha creat
  */
+/*
 module.exports.getOpenedPolls = async(id) => {
-   return 1;
-};
 
+    //Al ser el mateix cens serà igual que per User
+    return 1;
+};
+*/
 /**
  * PRE: Administador entra a la web
  * POST: json amb un llistat de les votacions tancades que ha creat
  */
+
+ /*
 module.exports.getClosedPolls = async(id) => {
+    //Al ser el mateix cens serà igual que per User
     return 1;
 };
+*/
+/************************************************************************************/
 
 /**
  * PRE: L'administrador ja ha creat la votació
  * POST: L'usuari amb clau pública 'publicKey' pot votar
  */
-module.exports.addVoterToPoll = async(id, priv) => {
-    let added = await ethervote.addVoter(id, priv);
-    if(added) return 1;
-    else return 0;
+module.exports.addVoter = async(key, priv) => {
+    var v1 = ethervote.getNumberOfVoters();
+    await ethervote.addVoter(key, priv);
+    var v2 = ethervote.getNumberOfVoters();
+    return (v1+1 == v2);
 };
 
 /**
@@ -108,9 +127,10 @@ module.exports.addVoterToPoll = async(id, priv) => {
  * POST: S'ha afegit una opció nova a la votació
  */
 module.exports.addOptionToPoll = async(id, name, description) => {
-    let opt = ethervote.addOption(id, name, description);
-    if (opt === -1) return 1;
-    else return 0;
+    var n1 = ethervote.getNumberOfOptions(id);
+    await ethervote.addOption(id, name, description);
+    var n2 = ethervote.getNumberOfOptions(id);
+    return (n1+1 == n2);
 };
 
 
@@ -144,6 +164,23 @@ module.exports.createSmartContract = async() => {
 
 /********************** POLL *********************/
 module.exports.getPoll = async(id) => {
+    let name = await ethervote.getProposalName(id);
+    let description = await ethervote.getProposalDescription(id);
+    let num_opcions = await ethervote.getNumberOfOptions(id);
+    let options = [];
+    for (let j=1; j<=num_opcions; ++j) {
+        let option_name = await ethervote.getOptionName(i, j);
+        let option_description = await ethervote.getOptionDescription(i, j);
+        let option_votes = await ethervote.getNumberOfVotes(i, j);
+
+        let o = {"name": option_name, "description": option_description, "votes": option_votes};
+        options.push(o);
+    }
+
+    let p = {"name": name, "description": description, "num_opcions": num_opcions, "options": options};
+
+
     console.log("FUNCIONA IMPORT");
+    //return p
     return 0;
 };
