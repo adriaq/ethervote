44d40ba9ed5bb747b372fb40de01d317426dbe0d
Jorge Ferrer Rodríguez <jferrer991@gmail.com>
2018-06-03 20:38:44 +0200

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,9 +7,31 @@ export default class Footer extends Component{
 
   render(){
     return(
-      <nav className="navbar fixed-bottom navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container">
-          <a className="navbar-brand-footer">Ethervote 2018 - A blockchain based voting system </a>
+      <nav class="navbar fixed-bottom navbar-expand-lg navbar-light bg-light navbar-footer-height" >
+        <div class="container">
+          <ul class="nav navbar-nav"><br/>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Register</a>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav mx-auto">
+            <li class="nav-item"><a class="nav-link" href="#">© 2018 Ethervote</a></li>
+          </ul>
+          <ul class="nav navbar-nav">
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">About us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Help</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
         </div>
       </nav>
     );
