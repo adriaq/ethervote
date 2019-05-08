5da56ba15cc1731d77fc3bede9476920f6456e43
Alba <alba.canete.garrucho@est.fib.upc.edu>
2019-05-08 16:12:59 +0200

    web3 functions in frontend

--- server/routes/audit_routes.js
+++
@@ -1,7 +0,0 @@
-import ethervote from '../app.js';
-
-exports.getAudit = function(req, res) {
-    var id = req.params.auditId;
-    console.log('Results of audit asked by user');
-    res.status(200).send('OK');
-};
