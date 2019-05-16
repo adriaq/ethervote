d5c2a9f0b5b4a0e751de6203b2aecec4193b3188
Alba <alba.canete.garrucho@est.fib.upc.edu>
2019-05-16 08:53:38 +0200

    pass address and not contract to components

--- frontend/src/Ethervote.js
+++ frontend/src/Ethervote.js
@@ -20,16 +20,16 @@ class Ethervote extends Component {
         var web3 = window.web3;
         try {
             // Request account access if needed
-             window.ethereum.enable();
+            window.ethereum.enable();
             // Acccounts now exposed
         } catch (error) {
             console.log(error);
-          }
+        }
         if(typeof web3 !== 'undefined'){
-          console.log("Using web3 detected from external source like Metamask")
-          this.web3 = new Web3(web3.currentProvider)
+            console.log("Using web3 detected from external source like Metamask")
+            this.web3 = new Web3(web3.currentProvider)
         }else{
-          this.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+            this.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
         }
 
         this.state = {
@@ -42,49 +42,49 @@ class Ethervote extends Component {
             deployed: null,
         };
     }
-     componentDidMount() {
-       this.web3.eth.getAccounts((error, accounts) => {
-           if (error) {
-               console.log(error)
-           } else {
-             var user_account = accounts[0];
-             this.setState({ user_address: user_account });
+    componentDidMount() {
+        this.web3.eth.getAccounts((error, accounts) => {
+            if (error) {
+                console.log(error)
+            } else {
+                var user_account = accounts[0];
+                this.setState({ user_address: user_account });
 
-             fetch('/get_ethervote')
-               .then(res => res.json())
-                  .then ((data) => {
-                    this.setState({ deployed: data.deployed });
-                    this.setState({ ethervote_address: data.ethervote_address });
-                    this.setState({ organitzation_name: data.organitzation_name });
+                fetch('/get_ethervote')
+                    .then(res => res.json())
+                    .then ((data) => {
+                        this.setState({ deployed: data.deployed });
+                        this.setState({ ethervote_address: data.ethervote_address });
+                        this.setState({ organitzation_name: data.organitzation_name });
 
-                    const ethervoteInstance = new this.web3.eth.Contract(ethervote_source.abi, data.ethervote_address);
-                    this.setState({ ethervote: ethervoteInstance });
+                        const ethervoteInstance = new this.web3.eth.Contract(ethervote_source.abi, data.ethervote_address);
+                        this.setState({ ethervote: ethervoteInstance });
 
-                    ethervoteInstance.methods.getPrivilege(user_account).call({from: this.state.user_address}).then(privilege_result => {
-                        this.setState({privilege: privilege_result.toString()});
+                        ethervoteInstance.methods.getPrivilege(user_account).call({from: this.state.user_address}).then(privilege_result => {
+                            this.setState({privilege: privilege_result.toString()});
+                        });
+
+                        ethervoteInstance.methods.owner.call({from: this.state.user_address}).then(owner_address => {
+                            if(user_account == owner_address) {this.setState({ owner: true })}
+                            else {this.setState({ owner: false })}
+
+                        });
+
+
                     });
-
-                    ethervoteInstance.methods.owner.call({from: this.state.user_address}).then(owner_address => {
-                      if(user_account == owner_address) {this.setState({ owner: true })}
-                      else {this.setState({ owner: false })}
-
-                    });
+            }
+        });
 
 
-              });
-           }
-       });
-
-
-     }
+    }
 
 
     render() {
         if(this.state.deployed === null) return (<div><img className="loading" src={loading} alt="loading"/></div>);
-        if (this.state.deployed == true) {
-            if(this.state.owner) {return <Admin web3={this.web3} ethervote={this.state.ethervote}/>;}
-            else if(this.state.privilege === 2) {return <User2 web3={this.web3} ethervote={this.state.ethervote}/>;}
-            else if(this.state.privilege === 1) {return <User web3={this.web3} ethervote={this.state.ethervote}/>;}
+        if (this.state.deployed === true) {
+            if(this.state.owner) {return <Admin web3={this.web3} ethervoteAddress={this.state.ethervote_address}/>;}
+            else if(this.state.privilege === 2) {return <User2 web3={this.web3} ethervoteAddress={this.state.ethervote_address}/>;}
+            else if(this.state.privilege === 1) {return <User web3={this.web3} ethervoteAddress={this.state.ethervote_address}/>;}
             else {return (<div><h1>usuari invalid</h1></div>);}
         } else {
             return <Firstlogin web3={this.web3}/>;
