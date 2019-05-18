c60ea98636df31a3653c057ea53ebbba997a6375
marb403 <marbadias97@gmail.com>
2019-05-18 18:34:33 +0200

    a openpoll nomes li queda que les opcions siguinn real i es vegin be

--- frontend/src/OpenPoll.js
+++ frontend/src/OpenPoll.js
@@ -13,25 +13,11 @@ import Ethervote from "./Ethervote";
 import swal from 'sweetalert';
 
 
-//FUNCIONS DE PROVA X RECONEIXER L'ELEMENT SELECIONAT
-/*function notify(el) {
-    resetElements();
-
-    console.log("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
-    console.log(el.innerHTML);
-    el.classList.add('active');
-}
-
-
-function findElement(id) {
-    var ele = document.getElementsByClassName(id);
-    notify(ele);
-}
-*/
+const ethervote_source = require('./contracts/ethervote.json');
 
 
 function PollListGroupItem(props) {
-
+    console.log('i get here');
     return (
         <ListGroupItem tag="button" onClick={(e) => props.getValue(props.title)} >
             <ListGroupItemHeading className="title"> {props.title} </ListGroupItemHeading>
@@ -44,152 +30,147 @@ function PollListGroupItem(props) {
 class OpenPoll extends Component {
     constructor(props) {
         super(props);
-        this.ethervote = this.props.ethervote;
         this.web3 = this.props.web3;
+        this.ethervoteAddress = this.props.ethervoteAddress;
+        this.ethervote = null;
         this.state = {
             Id: this.props.id,
-            opcionsPoll: [],
-            Selected: ' ',
-
-
-
-            // petit json montat x fer proves
-
-            candidats: [
+            name: '',
+            description: '',
+            opcionsPoll: [
                 {
-
-                    "id":"0",
-                    "name": "example glossary",
-                    "description": "Descripcio de el que votarem a continuacio",
-                    "num_opcions": "4",
-                    options: [
-                        {
-                            "id": "1",
-                            "name": "M. Rajoy",
-                            "description": "Luis se fuerte",
-                            "votes": "3",
-                         },
-                        {
-                            "id": "2",
-                            "name": "G. Rufian",
-                            "description": "Soc una mica demagog",
-                            "votes": "3",
-                        },
-                        {
-                            "id": "3",
-                            "name": "A. Rivera",
-                            "description": "Ibex 35",
-                            "votes": "3",
-                        } ],
+                    "id": "1",
+                    "name": "M. Rajoy",
+                    "description": "Luis se fuerte",
+                    "votes": "3",
                 },
+                {
+                    "id": "2",
+                    "name": "G. Rufian",
+                    "description": "Soc una mica demagog",
+                    "votes": "3",
+                }
+
             ],
-            
-            candidats: [],
-
-
+            Selected: ''
         };
-
-        this.vote = this.vote.bind(this);
         this.getValue = this.getValue.bind(this);
-        this.ether_vote = this.ether_vote.bind(this);
-
-        this.ether_getPoll = this.ether_getPoll.bind(this);
-
-        this.state.candidats = this.ether_getPoll();
-        console.log(this.state.candidats);
-
-        //Tira error no se xq
-        this.state.candidats.map( o => {this.state.opcionsPoll = o.options });
     }
 
-    async ether_getPoll() {
-        let name = await this.ethervote.getProposalName(this.state.Id);
-        let description = await this.ethervote.getProposalDescription(this.state.Id);
-        let num_opcions = await this.ethervote.getNumberOfOptions(this.state.Id);
-        let options = [];
-        for (let j = 1; j <= num_opcions; ++j) {
-            let option_name = await this.ethervote.getOptionName(this.state.Id, j);
-            let option_description = await this.ethervote.getOptionDescription(this.state.Id, j);
-            let option_votes = await this.ethervote.getNumberOfVotes(this.state.Id, j);
-
-            let o = {"id": j, "name": option_name, "description": option_description, "votes": option_votes};
-            options.push(o);
-        }
-
-        let p = {"name": name, "description": description, "num_opcions": num_opcions, "options": options};
-        return JSON.parse(p);
+    goToEth() {
+        console.log(this.web3);
+        ReactDOM.render(<Ethervote web3={this.web3} ethervoteAddress={this.ethervoteAddress}/>, document.getElementById('root'));
     }
 
-    async ether_vote() {
-        console.log("asdasdadsa");
-        let v = false;
-        let idPoll = this.state.Id;
+    componentDidMount() {
+        this.ethervote = new this.web3.eth.Contract(ethervote_source.abi, this.ethervoteAddress);
+        this.web3.eth.getAccounts((error, accounts) => {
+            if (error) {
+                console.log(error)
+            } else {
+                console.log(accounts);
+                console.log(accounts[0]);
+                let user_account = accounts[0];
+                this.setState({ user_address: user_account });
 
-        //Agafar id de la opció seleccionada
-        let idOption = 1;
-        let voted = await this.ethervote.hasVoted(this.web3.eth.accounts[0], idPoll); //retorna tal qual true or false
-        if(!voted) {
-            await this.ethervote.vote(idPoll, idOption); //no retorna res important
-            v = await this.ethervote.hasVoted(this.web3.eth.accounts[0], idPoll);
-        }
-        //v si false hi ha hagut un error,
-        //ja sigui xq ya ha votat o perque no ha pogut votar per algo extrany
-        return v;
-        console.log(v);
+                console.log(this.ethervote);
+                this.ethervote.methods.getProposalName(this.state.Id).call({from: this.state.user_address}).then(nom => {
+                    this.setState({
+                        name: nom,
+                    });
+                });
+                this.ethervote.methods.getProposalDescription(this.state.Id).call({from: this.state.user_address}).then(des => {
+                    this.setState({
+                        description: des,
+                    });
+                });
+
+            }
+        });
+
     }
 
 
     vote() {
-        swal({
-            title: "Are you sure? You are voting for " + this.state.Selected,
-            text: "Once you vote, you will not be able to make any change!",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-        })
-            .then((willDelete) => {
-                if (willDelete) {
-                    swal("You have voted successfuly!", {
-                        icon: "success",
-                    });
-
-                    //S'hauria de cridar abans de que surti com a succes, no?
-                    let asd = this.ether_vote();
-                    ReactDOM.render(<Ethervote/>, document.getElementById('root'));
-                    console.log(asd);
-                } else {
-                    swal("Canceled Vote");
+        console.log('funcio vote');
+        console.log(this.state.Selected);
+        if (this.state.Selected === ''){
+            swal({
+                title: "Error!",
+                text: 'Select one candidate',
+                icon: "warning",
+                button: {
+                    text: "Understood!",
+                    className: "botosweet"
                 }
             });
+        }
+        else {
+            swal({
+                title: "Are you sure? You are voting for " + this.state.Selected,
+                text: "Once you vote, you will not be able to make any change!",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            })
+                .then((willDelete) => {
+                    if (willDelete) {
+                        swal("Encara estem implementant la votacio!", {
+                            icon: "success",
+                        });
+
+                        //S'hauria de cridar abans de que surti com a succes, no?
+                        //let asd = this.ether_vote();
+                        ReactDOM.render(<Ethervote/>, document.getElementById('root'));
+                        //console.log(asd);
+                    } else {
+                        swal({
+                            title: "Vote canceled",
+                            icon: "warning",
+                            button: {
+                                text: "Understood!",
+                                className: "botosweet"
+                            }
+                        });
+                    }
+                });
+        }
     }
 
     //ganache-cli -d
-    getValue = (selected_value) => {
-        console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        console.log(selected_value);
-        this.setState({Selected: selected_value});
 
-        console.log("BBBBBBBBBBBBBBBBB");
-        console.log(this.state.Selected);
-    }
+
+    getValue = (selected_value) => {
+        /*
+        Atencio, la manera correcta de fer això seri a atraves de un setState
+            pero com el setState es asincron no funciona be. Sempre va un click per darrere.
+            i no podem permetre que es selecioni una opcio i sen voti una altra
+        * */
+        this.state.Selected = selected_value;
+    };
 
     render() {
 
+        let name = this.state.name;
+        let desc = this.state.description;
         return (
 
             <div>
                 <Header/>
                 <div>
-                    <p className="text"> {this.state.candidats.map(o => o.description)}</p>
+                    <div className='titol'>
+                        <h1>{name}</h1>
+                        <h4>{desc}</h4>
+                    </div>
                     <Col>
-                       <ListGroup className="votations">
-                           {this.state.opcionsPoll.map( o =>
-                               <PollListGroupItem tag="a" key={o.id} title={o.name} description={o.description} getValue={this.getValue} />)}
-                       </ListGroup>
+                        <ListGroup className="votations">
+                            {this.state.opcionsPoll.map( o =>
+                                <PollListGroupItem tag="a" key={o.id} title={o.name} description={o.description} getValue={this.getValue} />)}
+                        </ListGroup>
                     </Col>
                     <div className="opcions">
-                        <Button className="votar" color="success" onClick={this.vote}> VOTE </Button>
-                        <Button className="enrere" color="danger" href="/User"> Back </Button>
+                        <Button className="votar" color="success" onClick={() => this.vote()}> VOTE </Button>
+                        <Button className="enrere"  onClick={() => this.goToEth()}> Back </Button>
                     </div>
                 </div>
                 <Footer/>
@@ -202,6 +183,19 @@ class OpenPoll extends Component {
 export default OpenPoll;
 
 
+/*
+* <ListGroup className="votations">
+                            {this.state.opcionsPoll.map( o =>
+                                <PollListGroupItem tag="a" key={o.id} title={o.name} description={o.description} getValue={this.getValue} />)}
+                        </ListGroup>
+* */
+
+
+/* <ListGroup className="votations">
+                           {this.state.opcionsPoll.map( o =>
+                               <PollListGroupItem tag="a" key={o.id} title={o.name} description={o.description} getValue={this.getValue} />)}
+                       </ListGroup>*/
+
         /* <ListGroup className="votations">
 
                            {this.state.candidats.p.map( o =>
@@ -211,9 +205,3 @@ export default OpenPoll;
 
 
 
-/* Solucionat el problema del map*/
-/* Pot ser per pillar el this.state.prova.options -> crear function smart contract per agafar només opcions*/
-/*Funcio feta -> getPollOptions(id)*/
-/*{this.state.prova.map( function(o, i) {
-     return <PollListGroupItem key={i} title={o.name} description={o.description}/>;
- })}*/
