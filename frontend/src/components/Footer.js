4ecbc7e478056518c42554dcb139d03fa3c0be79
PennagrossoJr <wuyun@MacBook-Air-de-Yun.local>
2019-05-19 21:58:27 +0200

    ajustes css

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
 import {Button} from 'reactstrap';
 import '../styles/Footer.css'
+import swal from 'sweetalert';
 
+
+    let about_us = {
+          title: "About us",
+          text: "Estudiantes de la FIB",
+    };
+    let contact = {
+         title: "Contact",
+         text: "Para contactar con nosotros..."
+     };
 
 export default class Footer extends Component{
 
   render(){
     return(
-      <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-footer-height" >
+      <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-footer-height navbar51" >
         <div className="container">
           <ul className="nav navbar-nav lg-auto">
             <li className="nav-item"><a className="nav-link" href="#">© 2019 Ethervote</a></li>
@@ -15,11 +25,9 @@ export default class Footer extends Component{
           <ul className="nav navbar-nav">
 
             <li className="nav-item">
-              <a className="nav-link" href="#">About us</a>
-            </li>
+              <Button className="btn4" onClick={() => {swal(about_us)}}> About us </Button>            </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Contact</a>
-            </li>
+              <Button className="btn4" onClick={() => {swal(contact)}}> Contact </Button>            </li>
           </ul>
         </div>
       </nav>
